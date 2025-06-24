abc8aa660272963c88dd035fa4071fdbf8a34aa3
yanchanglong <yanchanglong@lf-network.com>
2025-06-24 18:12:55 +0800

    feat:企业站初始化

---
+++ LF_03/tailwind.config.js
@@ -0,0 +1,22 @@
+/// 自定义tailwind 设置
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                tabbar: ''
+            },
+            height: {
+                tabbar: '100px'
+            },
+            backgroundColor: {
+                tabbar: '#252429',
+                active: '#FF350D',
+                home: '#f6f6f6'
+            },
+            fontFamily: {
+                'microsoft-yahei': ['Microsoft YaHei', 'sans-serif']
+            }
+        }
+    }
+}
