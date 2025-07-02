5490486f8e4fee878837985960e235c276aee0d7
yanchanglong <yanchanglong@lf-network.com>
2025-07-02 09:36:03 +0800

    feat:完善04

--- LF_04/webpack.config.js
+++ LF_04/webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const HtmlMinimizerPlugin = require('html-minimizer-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+
+const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  devtool: 'source-map',
-  mode: 'development',
+  devtool: isProduction ? false : 'source-map',
+  mode: isProduction ? 'production' : 'development',
   entry: './src/index.tsx',
   output: {
-    filename: 'bundle.js',
+    filename: isProduction ? '[name].[contenthash].js' : '[name].js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    chunkFilename: isProduction ? '[name].[contenthash].chunk.js' : '[name].chunk.js',
   },
   module: {
     rules: [
@@ -21,7 +31,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
+        use: [
+          isProduction ? MiniCssExtractPlugin.loader : 'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
@@ -32,10 +46,81 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+      minify: isProduction ? {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
+      } : false,
     }),
+    ...(isProduction ? [
+      new MiniCssExtractPlugin({
+        filename: '[name].[contenthash].css',
+        chunkFilename: '[name].[contenthash].chunk.css',
+      }),
+      new CompressionPlugin({
+        test: /\.(js|css)$/,
+        algorithm: 'gzip',
+      }),
+      new BundleAnalyzerPlugin(),
+    ] : []),
   ],
   resolve: {
     extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  optimization: {
+    minimize: isProduction,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress: {
+            drop_console: true,
+            drop_debugger: true,
+            pure_funcs: ['console.log'],
+          },
+          output: {
+            comments: false,
+          },
+        },
+      }),
+      new CssMinimizerPlugin({
+        parallel: true
+      }),
+      new HtmlMinimizerPlugin(),
+    ],
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        react: {
+          test: /[\\/]node_modules[\\/](react|react-dom|react-router-dom)[\\/]/,
+          name: 'react',
+          chunks: 'all',
+          priority: 20,
+        },
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+          priority: 10,
+        },
+        common: {
+          name: 'common',
+          minChunks: 2,
+          chunks: 'all',
+          priority: 5,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+    runtimeChunk: {
+      name: 'runtime',
+    },
   },
   devServer: {
     static: {
